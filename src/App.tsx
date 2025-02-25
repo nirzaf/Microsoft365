381f51d9ecb553f685d7f6fb444be6cc2189984b
nirzaf <mfmfazrin1986@gmail.com>
2025-02-25 17:18:25 +0300

    🛠️ src/App.tsx -> Removed unused imports 🛠️ src/pages/Microsoft365AppsTable.tsx -> Simplified import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Microsoft365AppsTable from './pages/Microsoft365AppsTable'
+import './App.css'
 
 function App() {
   return (
--- src/pages/Microsoft365AppsTable.tsx
+++ src/pages/Microsoft365AppsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const Microsoft365AppsTable = () => {
   const [activeTab, setActiveTab] = useState('business');
